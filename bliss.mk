4c96ccb6c58c053b7b335639aed3d2ab018c5cb8
Silesh K Nair <sileshn@gmail.com>
2015-06-09 06:15:20 +0530

    Blissify

--- bliss.mk
+++ bliss.mk
@@ -15,25 +15,21 @@
 #
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/meliusltexx/full_meliusltexx.mk)
 
 # product
 PRODUCT_DEVICE := meliusltexx
 PRODUCT_BRAND := Samsung
-PRODUCT_NAME := cm_meliusltexx
+PRODUCT_NAME := bliss_meliusltexx
 PRODUCT_MODEL := GT-I9205
 PRODUCT_MANUFACTURER := samsung
 
 # override
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_NUMBER=I9205XXUDNE4 \
-    TARGET_DEVICE=meliuslte \
-    TARGET_BUILD_TYPE=user \
-    BUILD_VERSION_TAGS=release-keys \
     PRIVATE_BUILD_DESC="meliusltexx-user 4.4.2 KOT49H I9205XXUDNE4 release-keys" \
     BUILD_FINGERPRINT="samsung/meliusltexx/meliuslte:4.4.2/KOT49H/I9205XXUDNE4:user/release-keys"
